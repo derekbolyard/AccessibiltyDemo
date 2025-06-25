cc1bd65110fc4a7e3b63404f2b85c4f10e5a0a3c
derekbolyard <32370477+derekbolyard@users.noreply.github.com>
2025-06-25 17:00:56 -0400

    Remove favicon link from index

---
+++ fire-tagger/src/app/scan/scan.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-scan',
+  imports: [CommonModule],
+  templateUrl: './scan.html',
+  styleUrl: './scan.sass'
+})
+export class Scan {
+
+}
